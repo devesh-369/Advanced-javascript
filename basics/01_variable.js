d150617e882fd59b650318465502dc094331ee8b
Devesh Pathak <pathakdevesh2003@gmail.com>
2025-08-01 17:34:30 +0000

    basic codes to understand

---
+++ basics/01_variable.js
@@ -0,0 +1,31 @@
+console.log("This is the starting chapter in the js")
+console.log("now we will understad about the js and we will be building projects")
+console.log ("now lets understand about variables")
+let a = 369
+let b = 355
+console.log(a)
+console.log(b)
+let add = (a + b)
+console.log (add)
+console.log("this is the addition and now we will multiply")
+let mul = (a*b)
+console.log (mul)
+let A = ("devesh ")
+let B = ("patak")
+console.log (A+B)
+console.log("ha ha it can add two strings as well")
+console.log("now we will understand about let, const, and var / why do we generally use the keyword let")
+var i1 = ("7")
+var i2=("devesh")
+{
+    var i2 =("devesh pathak")
+    console.log(i2)
+}
+console.log (i2);
+console.log("see the variables is actually made globlay and can not be changed when its required")
+const int =5
+console.log(int)
+// now we can say that 
+// for any code we must try to use the let to declare the variable and while editing the prior codes we can use the var 
+console.log("heres everything yout need to know about the basics of the js and variables use of let const and var now lets mode to the other topics")
+
---
+++ basics/02_datatyps_objects.js
@@ -0,0 +1,3 @@
+console.log("Hi there we are again here to undstand about the data types and the opbjects in js and now are going to learn about datatypes heres a short trick to remember all the data trypes in js")
+console.log("N BB SS U")
+console.log ("here is short trick to learn the data types for the ease to rember")
