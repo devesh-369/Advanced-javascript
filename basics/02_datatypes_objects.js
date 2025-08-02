abbc08d7b4e0ff748d4527fdf8ec2a93786affa4
Devesh Pathak <pathakdevesh2003@gmail.com>
2025-08-02 15:45:19 +0000

    code done

---
+++ basics/02_datatypes_objects.js
@@ -0,0 +1,22 @@
+console.log("Hi there we are again here to undstand about the data types and the opbjects in js and now are going to learn about datatypes heres a short trick to remember all the data trypes in js")
+console.log("N BB SS U")
+let a = 456
+let b = null
+let c = true
+let d =  BigInt("666666")
+let e = "devesh"
+let f = Symbol("I am a nice symbol")
+let g = undefined
+console.log(a, b, c, d, e, f, g)
+console.log(typeof d)
+console.log(typeof c)
+console.log("this is used to find out the type of datatype")
+//its all about primitive data types and now we will move farward to objects in js
+const items = {
+    "devesh" : null,
+    "harry" : true,
+    "ceaden": false,
+}
+console.log("this is the non primitive data types")
+// we are good to go for the operators now
+
---
+++ basics/03_operators.js
@@ -0,0 +1,12 @@
+console.log("now we are going to arithmatic operator")
+let a = 44
+let b = 9
+console.log("a+b",  a+b)
+console.log("a-b",  a-b)
+console.log("a*b",  a*b)
+console.log("a%b",  a%b)
+console.log("a**b",  a**b)
+console.log("a/b",  a/b)
+console.log("this is the use of the arithmatic operator")
+
+
