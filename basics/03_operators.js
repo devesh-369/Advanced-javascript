adc6ad802154c065b4979efc1e814cd5e4778651
Devesh Pathak <pathakdevesh2003@gmail.com>
2025-08-11 09:33:34 +0000

    code done

--- basics/03_operators.js
+++ basics/03_operators.js
@@ -8,5 +8,23 @@ console.log("a%b",  a%b)
 console.log("a**b",  a**b)
 console.log("a/b",  a/b)
 console.log("this is the use of the arithmatic operator")
+console.log(" a++", a++)
+console.log(" these are all the arithmatic operator ")
+// now we will start assigniment operators
+a = 8
+console.log(a)
+let comp1 = 6;
+let comp2 = "6";
+console.log("comp1 == comp2", comp1 == comp2 )
+console.log ("comp1 === comp2", comp1 === comp2)
+
+// logical operator
+let x =5
+let y = 6
+
+
+
+
+
 
 
