fbd8d3a96250bd8b1149d3bb2777db2b975c8fcc
Devesh Pathak <pathakdevesh2003@gmail.com>
2025-08-19 07:33:48 +0000

    code done

---
+++ basics/04_loops.js
@@ -0,0 +1,16 @@
+console.log("hello here we will practice the loop in javascript");
+//to print a simple line for multiple times in programme we use the loops
+console.log("writting a programe to print hello world in js for 10 times");
+for( let i =1; i<=10;i++){
+    console.log("Hello world");
+}
+console.log("we have successfully run the programme here");
+//lets print the table and then see ; 
+for(let i = 1; i<= 10; i++){
+    for(let j = 1; j <= 10; j++){
+        console.log(i*j);
+    
+    }
+}
+console.log("end");
+console.log("")
